d642bb56b8f8b7895f048964ce0f9e8aaeb75ef4
nehas123 <nehasalankar@Nehas-MacBook-Pro.local>
2017-12-07 22:14:09 +0530

    Parsed data of given url and displayed in the tableview

--- Test/TViewController/ViewController.m
+++ Test/TViewController/ViewController.m
@@ -7,22 +7,30 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface ViewController ()
 {
     UITableView *tableview;
-
+    NSArray *arrResponse;
+    UIRefreshControl *refreshControl;
 }
 @end
 
 @implementation ViewController
+NSString *strUrl = @"https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
+#define kTableViewCellHeight 44.0
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
     //To add tableview
     [self createTableView];
-
+    
+    //Call Web Service
+    [self getWebData];
 }
 -(void) createTableView{
     tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
@@ -37,18 +45,18 @@
     [[NSLayoutConstraint constraintWithItem: tableview
                                   attribute: NSLayoutAttributeLeading
                                   relatedBy: NSLayoutRelationEqual
-                                  toItem: self.view
+                                     toItem: self.view
                                   attribute: NSLayoutAttributeLeading
-                                  multiplier: 1
-                                  constant: 0.0] setActive:true];
+                                 multiplier: 1
+                                   constant: 0.0] setActive:true];
     
     [[NSLayoutConstraint constraintWithItem:tableview
-                                attribute: NSLayoutAttributeTop
-                                relatedBy: NSLayoutRelationEqual
-                                toItem: self.view
-                                attribute: NSLayoutAttributeTop
-                                multiplier: 1
-                                constant: 0] setActive:true];
+                                  attribute: NSLayoutAttributeTop
+                                  relatedBy: NSLayoutRelationEqual
+                                     toItem: self.view
+                                  attribute: NSLayoutAttributeTop
+                                 multiplier: 1
+                                   constant: 0] setActive:true];
     
     NSLayoutConstraint *width = [NSLayoutConstraint
                                  constraintWithItem:tableview
@@ -71,25 +79,88 @@
     [self.view addConstraint:width];
     [self.view addConstraint:height];
 }
+-(void) getWebData{
+    NSURL *url = [NSURL URLWithString:strUrl];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSDictionary* responseDict = (NSDictionary *)responseObject;
+        NSArray *responseArray = [responseDict valueForKey:@"rows"];
+        arrResponse = [NSArray arrayWithArray:responseArray];
+        self.title = [responseDict objectForKey:@"title"];
+        [tableview reloadData];
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [self showAlert];
+    }];
+    
+    [operation start];
+}
+-(void) showAlert {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error Retrieving data" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+    }];
+    [alertController addAction:okAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 #pragma mark UITableView Datasource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return arrResponse.count;
 }
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellId = @"cellId";
     UITableViewCell *tableviewCell = [tableView dequeueReusableCellWithIdentifier:cellId];
     if(tableviewCell == nil){
         tableviewCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
     }
-    tableviewCell.textLabel.text = @"test";
+    tableviewCell.textLabel.numberOfLines = 0;
+    if([[arrResponse objectAtIndex:indexPath.row] valueForKey:@"title"] != [NSNull null]){
+        tableviewCell.textLabel.text = [[arrResponse objectAtIndex:indexPath.row] valueForKey:@"title"];
+    }
+    else{
+        tableviewCell.textLabel.text = @"No title";
+    }
+    tableviewCell.detailTextLabel.numberOfLines = 0;
+    
+    if ([[arrResponse objectAtIndex:indexPath.row] valueForKey:@"description"] != [NSNull null]){
+        tableviewCell.detailTextLabel.text = [[arrResponse objectAtIndex:indexPath.row] valueForKey:@"description"];
+    }
+    else{
+        tableviewCell.detailTextLabel.text = @"No description";
+    }
+    
+    if ([[arrResponse objectAtIndex:indexPath.row] valueForKey:@"imageHref"] != [NSNull null]) {
+        
+        __block UIImage *img;
+        __block UIImageView *imageView1 = tableviewCell.imageView;
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            
+        img = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString: [[arrResponse objectAtIndex:indexPath.row] valueForKey:@"imageHref"]]]];
+            
+        dispatch_async(dispatch_get_main_queue(),^{
+                
+        [imageView1 setImageWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[[arrResponse objectAtIndex:indexPath.row] valueForKey:@"imageHref"]]]
+                                  placeholderImage:[UIImage imageNamed:@"PlaceholderImg"]
+                                           success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+                                               
+                                               tableviewCell.imageView.image=image;
+                                           }
+                                           failure:nil];
+            });
+        });
+    }
+    else{
+        tableviewCell.imageView.image = [UIImage imageNamed:@"PlaceholderImg"];
+    }
     return tableviewCell;
 }
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
 @end
