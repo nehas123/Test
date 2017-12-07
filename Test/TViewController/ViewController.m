ba389a18ccd45f1472ce4e03e04cf20934bafaba
nehas123 <nehasalankar@Nehas-MacBook-Pro.local>
2017-12-07 21:03:10 +0530

    Added tableview

--- Test/TViewController/ViewController.m
+++ Test/TViewController/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UITableView *tableview;
 
+}
 @end
 
 @implementation ViewController
@@ -17,10 +20,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.title = @"Test";
+    //To add tableview
+    [self createTableView];
+
+}
+-(void) createTableView{
+    tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    tableview.delegate = self;
+    tableview.dataSource = self;
+    [self.view addSubview:tableview];
+    tableview.estimatedRowHeight = 44;
+    tableview.rowHeight = UITableViewAutomaticDimension;
+    
+    //Constraints
+    tableview.translatesAutoresizingMaskIntoConstraints = false;
+    [[NSLayoutConstraint constraintWithItem: tableview
+                                  attribute: NSLayoutAttributeLeading
+                                  relatedBy: NSLayoutRelationEqual
+                                  toItem: self.view
+                                  attribute: NSLayoutAttributeLeading
+                                  multiplier: 1
+                                  constant: 0.0] setActive:true];
+    
+    [[NSLayoutConstraint constraintWithItem:tableview
+                                attribute: NSLayoutAttributeTop
+                                relatedBy: NSLayoutRelationEqual
+                                toItem: self.view
+                                attribute: NSLayoutAttributeTop
+                                multiplier: 1
+                                constant: 0] setActive:true];
+    
+    NSLayoutConstraint *width = [NSLayoutConstraint
+                                 constraintWithItem:tableview
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:0
+                                 toItem:self.view
+                                 attribute:NSLayoutAttributeWidth
+                                 multiplier:1.0
+                                 constant:0];
+    
+    NSLayoutConstraint *height =[NSLayoutConstraint
+                                 constraintWithItem:tableview
+                                 attribute:NSLayoutAttributeHeight
+                                 relatedBy:0
+                                 toItem:self.view
+                                 attribute:NSLayoutAttributeHeight
+                                 multiplier:1.0
+                                 constant:0];
+    
+    [self.view addConstraint:width];
+    [self.view addConstraint:height];
 }
 
+#pragma mark UITableView Datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellId = @"cellId";
+    UITableViewCell *tableviewCell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if(tableviewCell == nil){
+        tableviewCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+    }
+    tableviewCell.textLabel.text = @"test";
+    return tableviewCell;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
